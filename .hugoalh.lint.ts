11638e9b7db69ebef13f292eb9d29f8e37194bba
hugoalh <hugoalh@gmail.com>
2025-04-03 14:09:27 +0800

    Update resources

--- .hugoalh.lint.ts
+++ .hugoalh.lint.ts
@@ -1,6 +1,10 @@
-import { configureDenoLintPlugin } from "https://raw.githubusercontent.com/hugoalh/deno-lint-rules/v0.4.0/mod.ts";
+import { configureDenoLintPlugin } from "HUGOALH_DENO_LINT_RULES";
 export default configureDenoLintPlugin({
+	"max-file-size": true,
 	"no-alert": true,
 	"no-confirm": true,
+	"no-iife": true,
+	"no-import-dynamic": true,
+	"no-import-npm": true,
 	"no-prompt": true
 }) satisfies Deno.lint.Plugin as Deno.lint.Plugin;
