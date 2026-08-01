8fd6bf87abab6295dd702892cc64b2acc6749501
hugoalh <hugoalh@gmail.com>
2026-08-01 18:03:56 +0800

    Improve CLI

--- tweetsodium/cli.ts
+++ tweetsodium/cli.ts
@@ -1,13 +1,14 @@
+import { parseArgs } from "node:util";
 import { GitHubSodiumSealer } from "../tweetsodium.ts";
 if (!import.meta.main) {
 	throw new Error(`This entrypoint is for command line only!`);
 }
-const args: readonly string[] = Deno.args;
-if (args.length !== 2) {
-	throw new Error(`Invalid arguments length! Expect: 2, Current: ${args.length}.`);
+const { positionals } = parseArgs({ allowPositionals: true });
+if (positionals.length !== 2) {
+	throw new Error(`Invalid arguments length! Expect: 2, Current: ${positionals.length}.`);
 }
 const [
 	publicKey,
 	secretValue
-]: readonly string[] = args;
+]: readonly string[] = positionals;
 console.log(new GitHubSodiumSealer(publicKey).encrypt(secretValue));
