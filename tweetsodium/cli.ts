6fb2af47afc16250908975f6a205a19b8cf90e79
hugoalh <hugoalh@gmail.com>
2026-08-03 12:25:40 +0800

    Improve CLI

--- tweetsodium/cli.ts
+++ tweetsodium/cli.ts
@@ -1,8 +1,27 @@
-import { parseArgs } from "node:util";
+#!/usr/bin/env -S deno run
+import { exit } from "node:process";
+import {
+	parseArgs,
+	styleText
+} from "node:util";
 import { GitHubSodiumSealer } from "../tweetsodium.ts";
 if (!import.meta.main) {
 	throw new Error(`This entrypoint is for command line only!`);
 }
+addEventListener("unhandledrejection", (event: PromiseRejectionEvent): void => {
+	event.preventDefault();
+	let message: string;
+	if (event.reason instanceof Error) {
+		message = event.reason.message;
+		if ((event.reason.stack ?? "").length > 0) {
+			message += `\n${event.reason.stack}`;
+		}
+	} else {
+		message = String(event.reason);
+	}
+	console.error(`${styleText(["red"], "ERR", { validateStream: false })}\t${message}`);
+	exit(1);
+}, { capture: true });
 const { positionals } = parseArgs({ allowPositionals: true });
 if (positionals.length !== 2) {
 	throw new Error(`Invalid arguments length! Expect: 2, Current: ${positionals.length}.`);
