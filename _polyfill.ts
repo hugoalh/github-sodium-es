4c90126f5d0947e0887255015e721457e6cd08ac
hugoalh <hugoalh@gmail.com>
2026-08-13 13:45:48 +0800

    Fmt

--- _polyfill.ts
+++ _polyfill.ts
@@ -1,8 +1,7 @@
+//deno-lint-ignore-file hugoalh/no-import-dynamic -- Polyfill.
 if (typeof Uint8Array.fromBase64 === "undefined") {
-	//deno-lint-ignore hugoalh/no-import-dynamic -- Polyfill.
 	await import("npm:es-arraybuffer-base64@^1.1.2/Uint8Array.fromBase64/auto");
 }
 if (typeof Uint8Array.prototype.toBase64 === "undefined") {
-	//deno-lint-ignore hugoalh/no-import-dynamic -- Polyfill.
 	await import("npm:es-arraybuffer-base64@^1.1.2/Uint8Array.prototype.toBase64/auto");
 }
